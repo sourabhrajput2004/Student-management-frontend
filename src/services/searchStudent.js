04f3025aba8b0866a58203fca3f1636ec454ac93
Sourabh Singh <ssaroubh2002@gmail.com>
2026-03-05 02:17:40 +0530

    fix: searchstudent added vite url

--- src/services/searchStudent.js
+++ src/services/searchStudent.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const searchStudents = (type, keyword) => {
   return axios.get(`${API_URL}/search`, {
